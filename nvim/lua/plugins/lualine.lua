72b146c03bc9c179dbd71a9eb41a1efe6b7e6016
GDDario <gd.dario.dev@gmail.com>
2025-09-02 00:54:45 -0300

    nvim: update lualine theme to vesper

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,8 @@ return {
     config = function()
         require("lualine").setup({
             options = {
-                theme = "gruvbox-material"
+                -- theme = "gruvbox-material"
+                theme = "vesper"
             }
         })
     end
--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,22 +1,22 @@
 return {
-    { 
-        "catppuccin/nvim", 
-        name = "catppuccin",
-        priority = 1000 
-    },
-    { 
-        "datsfilipe/vesper.nvim", 
-        name = "vesper", 
-        priority = 1000,
-        opts = {
-            italics = {
-                strings = false,
-                comments = true,
-            },
-        },
-        config = function(_, opts)
-            require("vesper").setup(opts)
-            vim.cmd.colorscheme "vesper"
-        end,
-    }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"datsfilipe/vesper.nvim",
+		name = "vesper",
+		priority = 1000,
+		opts = {
+			italics = {
+				strings = false,
+				comments = true,
+			},
+		},
+		config = function(_, opts)
+			require("vesper").setup(opts)
+			vim.cmd.colorscheme("vesper")
+		end,
+	},
 }
