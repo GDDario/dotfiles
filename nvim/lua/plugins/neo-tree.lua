1ff4bebd9d04c67315ca857e3fb65c9298ee8285
GDDario <gd.dario.dev@gmail.com>
2025-09-02 12:53:44 -0300

    nvim: configure neo-tree and telescope to show hidden files by default

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,16 +1,25 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            "nvim-tree/nvim-web-devicons",
-        },
-        lazy = false,
-        toggle = true,
-        config = function()
-            vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-        end
-    }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false,
+		toggle = true,
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					filtered_items = {
+						visible = true,
+						hide_dotfiles = false,
+					},
+				},
+			})
+
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		end,
+	},
 }
