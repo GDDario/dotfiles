69c62e6d5c61579b5277cf760b65439e1b2f42e7
GDDario <gd.dario.dev@gmail.com>
2025-09-02 15:29:33 -0300

    nvim: configure intelephense lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -15,41 +15,62 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
-        lazy = false,
+		lazy = false,
 		config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" }
-                        }
-                    },
-                    telemetry = {
-                        enable = false
-                    }
-                }
-            })
-			lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-			-- lspconfig.phpactor.setup({
-            --    capabilities = capabilities,
-            -- })
-            lspconfig.intelephense.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.angularls.setup({
-                capabilities = capabilities
-            })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			vim.keymap.set("n", "K", function()
+				local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+				for _, client in ipairs(clients) do
+					if client.server_capabilities.hoverProvider then
+						vim.lsp.buf.hover({ client_id = client.id })
+						break
+					end
+				end
+			end, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-            vim.diagnostic.config({ virtual_text = true })
+			local on_attach = function(client, bufnr) end
+
+			local servers = {
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = { globals = { "vim" } },
+							telemetry = { enable = false },
+						},
+					},
+				},
+				ts_ls = {},
+				angularls = {},
+				laravel_ls = {},
+				intelephense = {
+					commands = {
+						IntelephenseIndex = {
+							function()
+								vim.lsp.buf.execute_command({ command = "intelephense.index.workspace" })
+							end,
+						},
+					},
+					settings = {
+						intelephense = {
+							environment = {
+								includePaths = { "./vendor" },
+							},
+						},
+					},
+				},
+			}
+
+			for server, config in pairs(servers) do
+				config.capabilities = capabilities
+				config.on_attach = on_attach
+				lspconfig[server].setup(config)
+			end
+
+			vim.diagnostic.config({ virtual_text = true })
 		end,
 	},
 	{
