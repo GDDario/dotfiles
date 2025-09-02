98b7393dbfa6a30edf8b524b4ded3981f79f83aa
GDDario <gd.dario.dev@gmail.com>
2025-09-02 11:53:39 -0300

    nvim: add key binds to comment to ctrl+/

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,5 +1,12 @@
 return {
-    'numToStr/Comment.nvim',
-    opts = {
-    },
+    "numtostr/comment.nvim",
+    opts = {},
+    config = function(_, opts)
+        local comment = require("Comment")
+        comment.setup(opts)
+
+        vim.keymap.set("n", "<C-_>", "<Plug>(comment_toggle_linewise_current)", { noremap = false, silent = true })
+
+        vim.keymap.set("x", "<C-_>", "<Plug>(comment_toggle_linewise_visual)", { noremap = false, silent = true })
+    end,
 }
