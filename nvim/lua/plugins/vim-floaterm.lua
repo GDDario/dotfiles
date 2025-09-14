e551140baada79b3a471e047ce8fd9f4b0bc50ab
GDDario <gd.dario.dev@gmail.com>
2025-09-14 16:55:05 -0300

    nvim: open lazygit open nvim directory
    Example: when in ~/project-x location, using `nvim ~/project-y` and then
    ctrl+g opens lazyvim on project-y.

--- nvim/lua/plugins/vim-floaterm.lua
+++ nvim/lua/plugins/vim-floaterm.lua
@@ -1,5 +1,8 @@
 return {
     "voldikss/vim-floaterm",
-    vim.keymap.set("n", "<C-g>", ":FloatermNew --height=0.9 --width=0.9 --wintype=float lazygit<CR>", {}),
+    vim.keymap.set("n", "<C-g>", function()
+        local dir = vim.fn.expand("%:p:h")
+        vim.cmd("FloatermNew --height=0.9 --width=0.9 --wintype=float cd " .. dir .. " && lazygit")
+    end, {}),
     -- vim.keymap.set("n", "<C-d>", ":FloatermNew --height=0.9 --width=0.9 --wintype=float lazydocker<CR>", {}),
 }
